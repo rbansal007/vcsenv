f82a21bd3bf54071807986a11077cc6f3999b83b
Ramit Bansal <ramit.bansal@hashicorp.com>
2024-08-14 16:17:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
-output "repo_id" {
-  value = coalesce(
-    nonsensitive(trimspace(var.repo_id)),
-    "No repository ID found"
-  )
-}
-
-variable "repo_id" {
+variable "git_branch" {
   type    = string
   default = ""
+}
+
+output "branch" {
+  value = coalesce(
+    nonsensitive(trimspace(var.git_branch)),
+    "No GIT branch found"
+  )
 }
