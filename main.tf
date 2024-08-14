8733b6e2a7a1166643e522ae25a0d17f83719b68
Ramit Bansal <ramit.bansal@hashicorp.com>
2024-08-14 16:34:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,20 @@
 provider "null" {}
 
-# Output the Git branch name directly from the environment variable
+# Output the Git branch name directly from the TFC_CONFIGURATION_VERSION_GIT_BRANCH variable
 output "git_branch_name" {
-  value = env("TFC_CONFIGURATION_VERSION_GIT_BRANCH")
+  value = var.git_branch
+}
+
+# Declare a variable to hold the Git branch name
+variable "git_branch" {
+  description = "The Git branch connected to the workspace"
+  type        = string
+  default     = ""
 }
 
 # Optionally, you can add a resource to demonstrate usage
 resource "null_resource" "example" {
   provisioner "local-exec" {
-    command = "echo The Git branch connected to the workspace is: ${env("TFC_CONFIGURATION_VERSION_GIT_BRANCH")}"
+    command = "echo The Git branch connected to the workspace is: ${var.git_branch}"
   }
 }
