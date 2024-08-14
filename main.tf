af98b25919cc42c8b1305f6a87c30f875bc8d5b9
Ramit Bansal <ramit.bansal@hashicorp.com>
2024-08-14 16:29:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,25 +1,13 @@
 provider "null" {}
 
-# Get the Git branch from the environment variable
-variable "git_branch" {
-  description = "The Git branch connected to the workspace"
-  type        = string
-  default     = ""
-}
-
-# Use the environment variable if set
-locals {
-  branch_name = length(var.git_branch) > 0 ? var.git_branch : (lookup(var, "TFC_CONFIGURATION_VERSION_GIT_BRANCH", "not set"))
-}
-
-# Output the Git branch name
+# Output the Git branch name directly from the environment variable
 output "git_branch_name" {
-  value = local.branch_name
+  value = env("TFC_CONFIGURATION_VERSION_GIT_BRANCH")
 }
 
 # Optionally, you can add a resource to demonstrate usage
 resource "null_resource" "example" {
   provisioner "local-exec" {
-    command = "echo The Git branch connected to the workspace is: ${local.branch_name}"
+    command = "echo The Git branch connected to the workspace is: ${env("TFC_CONFIGURATION_VERSION_GIT_BRANCH")}"
   }
 }
